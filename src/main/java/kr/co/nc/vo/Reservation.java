da98ad5a6c0d1154e8cbd24eebc0272e716768d7
JaeYong <JaeYong@me.com>
2022-08-11 18:05:11 +0900

    예약객체는 방이름 추가or다른테이블이랑 연동가능한지 확인할것 결제객체는 foreach문 다중으로 사용하는거 알아보기

--- src/main/java/kr/co/nc/vo/Reservation.java
+++ src/main/java/kr/co/nc/vo/Reservation.java
@@ -2,7 +2,6 @@ package kr.co.nc.vo;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 
 
 import lombok.Getter;
@@ -14,24 +13,17 @@ import lombok.ToString;
 @ToString
 public class Reservation {
 
-	private String impUid;
-	private String merchantUid;
+	private String reservationNo;
 	private String reserName;
 	private String reserTel;
 	private String amount;
 	private String checkIn;
 	private String checkOut;
-	private String pgProvider;
 	private String paymentStatus;
 	private String reservationStatus;
 	
-	public static Reservation saveReservation(PaymentRequest paymentRequest) {
-		Reservation reservation = new Reservation();
-		BeanUtils.copyProperties(paymentRequest, reservation);
-		return reservation;
-	}
 	
-	private List<User> users;
-	private List<Accommodation> accomoAccommodations;
-	private List<AccommodationRoom> accommodationRooms;
+	private User users;
+	private Accommodation acco;
+	private AccommodationRoom Room;
 }
