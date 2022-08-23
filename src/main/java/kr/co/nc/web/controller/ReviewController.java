7330ad901c92b0500f0d1e4d22650516dec513ad
JaeYong <JaeYong@me.com>
2022-08-23 16:57:27 +0900

    되돌리기

--- src/main/java/kr/co/nc/web/controller/ReviewController.java
+++ src/main/java/kr/co/nc/web/controller/ReviewController.java
@@ -1,21 +1,11 @@
 package kr.co.nc.web.controller;
 
-import java.io.IOException;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import kr.co.nc.annotation.LoginUser;
 import kr.co.nc.service.ReviewService;
-import kr.co.nc.web.form.ReviewRegisterForm;
-import lombok.extern.slf4j.Slf4j;
-import kr.co.nc.vo.User;
 
-@Slf4j
 @Controller
 public class ReviewController {
 
@@ -30,16 +20,12 @@ public class ReviewController {
 	
 	// 리뷰 입력폼
 	@GetMapping(path = "/reviewform")
-	public String ReviewForm(Model model)  {
-
-		model.addAttribute("form", new ReviewRegisterForm());
-		
-		return "reviews/reviewform";
+	public String ReviewForm() {
+		return "reviews/form";
 	}
 	
-	@PostMapping(path = "/reviewcomplete")
-	public String reviewCompleted(@LoginUser User user , Model model) {		
-		
-		return "redirect:/reviews/reviewcomplete";
+	@GetMapping(path = "/reviewcomplete")
+	public String reviewCompleted() {
+		return "reviews/reviewcomplete";
 	}
 }
