ba9c2effa70e5e24dc44946fc992338341e5ab66
yeseok <lys20933@naer.com>
2022-08-24 14:40:57 +0900

    작동 오류 수정

--- src/main/java/kr/co/nc/service/UserService.java
+++ src/main/java/kr/co/nc/service/UserService.java
@@ -3,7 +3,6 @@ package kr.co.nc.service;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.co.nc.mapper.UserMapper;
--- src/main/java/kr/co/nc/web/controller/HomeController.java
+++ src/main/java/kr/co/nc/web/controller/HomeController.java
@@ -1,20 +1,16 @@
 package kr.co.nc.web.controller;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
